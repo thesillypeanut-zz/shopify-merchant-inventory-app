ac7b98d26cb7efef0b17d688f4180d145b74a513
Maliha Islam <maliha.rh@gmail.com>
2019-01-08 20:57:52 -0500

    Initial Commit

---
+++ shopify-merchant-inventory-app/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  shopify-merchant-inventory-app
+//
+//  Created by Maliha Islam on 2019-01-08.
+//  Copyright © 2019 Maliha Islam. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
